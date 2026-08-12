b4699985d8569f30fd259969eeda98fa68fb5ac9
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-12 17:07:01 -0400

    Run chain control propagation tests

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -5,6 +5,7 @@
 :- consult(rlm_chain_test).
 :- consult(rlm_chain_runtime_test).
 :- consult(rlm_chain_message_metadata_test).
+:- consult(rlm_chain_control_test).
 :- consult(rlm_context_test).
 :- consult(rlm_plan_test).
 :- consult(rlm_tool_test).
