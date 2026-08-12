a4c316b5768ce16a674e8ec7b00def8d253e74c3
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-11 21:28:02 -0400

    Add rlm_graph module skeleton for #4

---
+++ prolog/rlm_graph.pl
@@ -0,0 +1,11 @@
+:- module(rlm_graph,
+          [ rlm_graph_ready/0
+          ]).
+
+/** <module> Durable graph runtime
+
+Future work adds validated state graphs, reducers, checkpoints, interrupts,
+replay, and cancellation here.
+*/
+
+rlm_graph_ready.
