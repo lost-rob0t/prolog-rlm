d86c75485412004ebebf28a427c6e56a0850f044
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-12 11:48:37 -0400

    Separate structured repair from core live suite

--- test/run_live_openrouter.pl
+++ test/run_live_openrouter.pl
@@ -5,14 +5,12 @@
 :- consult(live_plan_openrouter_test).
 :- consult(live_tool_openrouter_test).
 :- consult(live_completion_openrouter_test).
-:- consult(live_repair_openrouter_test).
 
 main(_) :-
     (   run_tests([live_openrouter,
                    live_plan_openrouter,
                    live_tool_openrouter,
-                   live_completion_openrouter,
-                   live_repair_openrouter])
+                   live_completion_openrouter])
     ->  halt(0)
     ;   halt(1)
     ).
