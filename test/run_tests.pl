272b133ac9803222f7029d8d529ef6b4c1401693
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-12 16:58:32 -0400

    Run canonical message metadata tests

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -4,6 +4,7 @@
 :- consult(bootstrap_test).
 :- consult(rlm_chain_test).
 :- consult(rlm_chain_runtime_test).
+:- consult(rlm_chain_message_metadata_test).
 :- consult(rlm_context_test).
 :- consult(rlm_plan_test).
 :- consult(rlm_tool_test).
