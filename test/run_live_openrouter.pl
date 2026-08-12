64a3bebaf2919e4c0f6f88f0612a4f5b74502d69
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-12 07:28:12 -0400

    Run real capability-tool suite in live CI

--- test/run_live_openrouter.pl
+++ test/run_live_openrouter.pl
@@ -3,9 +3,12 @@
 :- use_module(library(plunit)).
 :- consult(live_openrouter_test).
 :- consult(live_plan_openrouter_test).
+:- consult(live_tool_openrouter_test).
 
 main(_) :-
-    (   run_tests([live_openrouter, live_plan_openrouter])
+    (   run_tests([live_openrouter,
+                   live_plan_openrouter,
+                   live_tool_openrouter])
     ->  halt(0)
     ;   halt(1)
     ).
