389f7e77b4c6efe594db11a2e2935c95cc3901b9
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-13 02:35:58 -0400

    test(recursion): wire deep experiment suite

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -27,6 +27,7 @@
 :- consult(rlm_recursion_runtime_test).
 :- consult(rlm_benchmark_test).
 :- consult(rlm_conformance_test).
+:- consult(rlm_deep_experiment_test).
 :- consult(rlm_trace_test).
 :- consult(rlm_demo_test).
 :- consult(rlm_cli_test).
