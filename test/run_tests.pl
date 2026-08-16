234d70a4ea91f2e8544266e2ba46be7a501eb534
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-16 19:27:19 -0400

    Include agent authority tests in canonical suite

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -27,6 +27,7 @@
 :- consult(rlm_outcome_test).
 :- consult(rlm_artifact_test).
 :- consult(rlm_agent_test).
+:- consult(rlm_agent_authority_test).
 :- consult(rlm_graph_test).
 :- consult(rlm_agent_graph_async_test).
 :- consult(rlm_recursion_policy_test).
