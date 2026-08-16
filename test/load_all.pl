a15d441afe7f6df2f3d14508f0f3c39a3c64289a
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-16 10:07:34 -0400

    Static-load external tool loader boundary

--- test/load_all.pl
+++ test/load_all.pl
@@ -1,6 +1,9 @@
 :- initialization(main, main).
 
 :- use_module('../prolog/rlm').
+:- use_module('../prolog/rlm_tool_loader', []).
+:- use_module('../prolog/rlm_mcp_server', []).
+:- use_module('../prolog/rlm_mcp_tool', []).
 
 main(_) :-
     (   rlm:rlm_ready
