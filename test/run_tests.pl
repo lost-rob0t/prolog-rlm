469c9e412dc52c246369f27161668260255e0b7b
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-17 14:09:14 -0400

    Run canonical effect executor tests

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -25,6 +25,7 @@
 :- consult(rlm_authority_lifecycle_test).
 :- consult(rlm_effect_test).
 :- consult(rlm_effect_authority_test).
+:- consult(rlm_effect_executor_test).
 :- consult(rlm_effect_restart_test).
 :- consult(run_tool_mcp_async_tests).
 :- consult(rlm_tool_mcp_scheduler_test).
