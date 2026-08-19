1a7d60ea617f151526a197b82fad59d7da090af5
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-19 00:38:07 -0400

    Run skill compiler tests in deterministic suite

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -27,6 +27,7 @@
 :- consult(rlm_tool_effect_test).
 :- consult(rlm_tool_loader_test).
 :- consult(rlm_tool_loader_security_test).
+:- consult(rlm_skill_test).
 :- consult(rlm_authority_test).
 :- consult(rlm_authority_hardening_test).
 :- consult(rlm_authority_lifecycle_test).
