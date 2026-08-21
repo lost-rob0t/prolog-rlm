97b7ad28fc4043425146a95187ab46733ec5e38c
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-21 09:34:51 -0400

    diagnostic: continue past red units until first timeout

--- test/run_tests_diagnostic.pl
+++ test/run_tests_diagnostic.pl
@@ -3,39 +3,54 @@
 
 :- consult(run_tests).
 
+:- dynamic diagnostic_failure/2.
+
 :- initialization(diagnostic_main, main).
 
 diagnostic_main :-
-    findall(Unit, plunit:current_test_unit(Unit, _), Units0),
-    sort(Units0, Units),
+    retractall(diagnostic_failure(_, _)),
+    findall(Unit, plunit:current_test_unit(Unit, _), Units),
     length(Units, UnitCount),
     format(user_error, 'diagnostic_unit_count=~d~n', [UnitCount]),
     flush_output(user_error),
     (   forall(member(Unit, Units), run_unit_bounded(Unit))
-    ->  halt(0)
+    ->  findall(Unit-Result, diagnostic_failure(Unit, Result), Failures),
+        format(user_error, 'diagnostic_failures=~q~n', [Failures]),
+        flush_output(user_error),
+        ( Failures == [] -> halt(0) ; halt(1) )
     ;   halt(1)
     ).
 
 run_unit_bounded(Unit) :-
     format(user_error, 'diagnostic_unit_start=~w~n', [Unit]),
     flush_output(user_error),
-    (   catch(call_with_time_limit(20, run_tests(Unit)),
-              Exception,
-              diagnostic_exception(Unit, Exception))
-    ->  format(user_error, 'diagnostic_unit_done=~w~n', [Unit]),
-        flush_output(user_error)
-    ;   format(user_error, 'diagnostic_unit_failed=~w~n', [Unit]),
-        flush_output(user_error),
-        fail
-    ).
+    unit_result(Unit, Result),
+    report_unit_result(Unit, Result),
+    Result \== timeout.
 
-diagnostic_exception(Unit, time_limit_exceeded) :-
+unit_result(Unit, Result) :-
+    catch(( call_with_time_limit(20, run_tests(Unit))
+          -> Result = pass
+          ;  Result = failed
+          ),
+          Exception,
+          diagnostic_exception_result(Exception, Result)).
+
+diagnostic_exception_result(time_limit_exceeded, timeout) :- !.
+diagnostic_exception_result(Exception, exception(Exception)).
+
+report_unit_result(Unit, pass) :-
+    !,
+    format(user_error, 'diagnostic_unit_done=~w~n', [Unit]),
+    flush_output(user_error).
+report_unit_result(Unit, timeout) :-
+    !,
+    assertz(diagnostic_failure(Unit, timeout)),
     format(user_error, 'diagnostic_unit_timeout=~w~n', [Unit]),
-    flush_output(user_error),
-    fail.
-diagnostic_exception(Unit, Exception) :-
+    flush_output(user_error).
+report_unit_result(Unit, Result) :-
+    assertz(diagnostic_failure(Unit, Result)),
     format(user_error,
-           'diagnostic_unit_exception=~w exception=~q~n',
-           [Unit, Exception]),
-    flush_output(user_error),
-    fail.
+           'diagnostic_unit_failed=~w result=~q~n',
+           [Unit, Result]),
+    flush_output(user_error).
