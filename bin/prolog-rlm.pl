89fd8d2c97b48121077f6eb95057a9f0f1c7d2d9
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-26 04:55:20 -0400

    feat: route bundled binary through reference CLI harness

--- bin/prolog-rlm.pl
+++ bin/prolog-rlm.pl
@@ -1,7 +1,7 @@
 :- initialization(main, main).
 
-:- use_module('../prolog/rlm_cli').
+:- use_module('../prolog/rlm_cli_app').
 
 main(Argv) :-
-    rlm_cli:cli_execute(Argv, ExitCode),
+    rlm_cli_app:cli_execute(Argv, ExitCode),
     halt(ExitCode).
