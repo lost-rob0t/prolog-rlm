f24e317905bc8ce37b03c72147f21836015a19d5
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-18 19:46:36 -0400

    #459 register expert advice tests

--- test/deterministic_corpus.pl
+++ test/deterministic_corpus.pl
@@ -165,6 +165,7 @@ corpus_entry('rlm_spec_lang_test.pl', include(rlm_spec_lang)).
 corpus_entry('rlm_spec_workflow_test.pl', include(rlm_spec_workflow)).
 corpus_entry('rlm_spec_strategy_test.pl', include(rlm_spec_strategy)).
 corpus_entry('rlm_reasoning_mode_test.pl', include(rlm_reasoning_mode)).
+corpus_entry('rlm_expert_advice_test.pl', include(rlm_expert_advice)).
 corpus_entry('rlm_project_source_test.pl', include(rlm_project_source)).
 corpus_entry('rlm_project_grammar_pack_test.pl',
               include(rlm_project_grammar_pack)).
